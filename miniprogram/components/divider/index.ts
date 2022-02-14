efa5c780812b5dbd6ed43d6e8e57fd179b6928de
jexlau <361684727@qq.com>
2022-02-14 16:49:24 +0800

    feat: add divider component

---
+++ miniprogram/components/divider/index.ts
@@ -0,0 +1,39 @@
+Component({
+  externalClasses: ['line-class'],
+  options:{
+    styleIsolation: 'apply-shared'
+  },
+  properties: {
+    /** 分割线方向：horizontal/vertical */
+    direction: {
+      type: String,
+      value: 'horizontal'
+    },
+    // 文案
+    content: {
+      type: String,
+      value: ''
+    },
+    /** 文案位置 */
+    // 文案位置 left/right/center/''
+    contentPosition: {
+      type: String,
+      value: 'center'
+    },
+    background: {
+      type: String,
+      value: '#fff'
+    },
+    marginTop: {
+      type: Number,
+      value: 30
+    },
+    marginBottom: {
+      type: Number,
+      value: 30
+    }
+  },
+  data: {}
+})
+
+export { }
